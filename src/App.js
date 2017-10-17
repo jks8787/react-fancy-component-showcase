e9fe7d77bda7672402ad19f85acf1d7c31c69f2b
jks8787 <janice.k.n.smith@gmail.com>
2017-10-17 09:28:20 -0400

    now using version 0.0.3 of react-fancy-lib

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'react-fancy-lib/build/style.css';
-import { Button, FancyMessage } from 'react-fancy-lib';
+import { Button, FancyMessage, Modal } from 'react-fancy-lib';
 
 class App extends Component {
+  componentWillMount() {
+    this.state = {
+      showModal: false
+    };
+  }
+
+  handleTriggerClick() {
+    this.setState({showModal: true});
+  }
+
+  handleClose() {
+    console.log('i am close');
+    this.setState({showModal: false});
+
+  }
   render() {
     return (
       <div className="App">
@@ -12,11 +27,27 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">This is a Component Showcase for react-fancy-lib!</h1>
         </header>
-        <div className="showcase">
-        <h2>Button Component</h2>
-        <Button />
-        <h2>FancyMessage Component</h2>
-        <FancyMessage />
+        <div>
+          <div className="fancy-lib-showcase-wrap">
+            <h2 className="title is-3">Button Component</h2>
+            <Button />
+          </div>
+          <div className="fancy-lib-showcase-wrap">
+            <h2 className="title is-3">FancyMessage Component</h2>
+            <FancyMessage />
+          </div>
+          <div className="fancy-lib-showcase-wrap">
+            <h2 className="title is-3">Modal</h2>
+            <button onClick={this.handleTriggerClick.bind(this)}>
+              Trigger
+            </button>
+            <Modal
+              isActive={this.state.showModal}
+              onClose={this.handleClose.bind(this)}
+              simpleMessageList={['Hi','I am a Modal']}
+              desiredClassName={"fancy-lib-modal"}
+            />
+          </div>
         </div>
       </div>
     );
